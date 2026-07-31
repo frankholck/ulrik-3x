a46e5b1b612d4f9e1fbf121ea9e2d5c492313c54
frankholck <58175428+frankholck@users.noreply.github.com>
2026-07-31 12:17:39 +0200

    Add offline app shell cache

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='ulrik-3x-v3';
+const FILES=['./','./index.html','./styles.css','./js/app-data.js','./js/app-core.js','./js/app-workouts.js','./js/app-nutrition-progress.js','./js/app-settings.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
